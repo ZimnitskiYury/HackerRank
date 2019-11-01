9f1272a4b695f170ed15804fe90f154d7b275ae0
Юрий Зимницкий <demovie.inc@live.com>
2019-11-02 01:00:55 +0300

    Поправил тип переменной для факториала, чтобы считать большие числа.

--- HackerRank/Extra Long Factorials/Extra_Long_Factorials.cs
+++ HackerRank/Extra Long Factorials/Extra_Long_Factorials.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Numerics;
 namespace Extra_Long_Factorials
 {
     class Extra_Long_Factorials
@@ -11,8 +11,18 @@ namespace Extra_Long_Factorials
         // Complete the extraLongFactorials function below.
         static void ExtraLongFactorials(int n)
         {
-
-
+            BigInteger factorial;
+            factorial = 1;
+            if (n < 0)
+                factorial = 0;
+            else if (n == 0)
+                factorial = 1;
+            else if (n == 1)
+                factorial = 1;
+            else if (n > 1)
+                for (int i = n; i > 1; i--)
+                    factorial *= i;
+            Console.WriteLine(factorial);
         }
 
         static void Main(string[] args)
