1567c03485af19f0679f9425c769e45133fc0cef
Юрий Зимницкий <demovie.inc@live.com>
2019-11-02 00:27:43 +0300

    Задача по факториалу

---
+++ HackerRank/Extra Long Factorials/Extra_Long_Factorials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_Long_Factorials
+{
+    class Extra_Long_Factorials
+    {
+        // Complete the extraLongFactorials function below.
+        static void ExtraLongFactorials(int n)
+        {
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            ExtraLongFactorials(n);
+        }
+    }
+}
